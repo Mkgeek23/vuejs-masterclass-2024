8d8598bc581e9fc838c74c242e92285255e502a3
Mkgeek23 <emkgeek@gmail.com>
2025-03-12 09:42:26 +0100

    Secure supabase keys

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://jbglmdvilkkblhfhwefs.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiZ2xtZHZpbGtrYmxoZmh3ZWZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3NjcxMTMsImV4cCI6MjA1NzM0MzExM30.YKDpV9xIG6U4sAoSfGBZGlXemStBmL-8X8hsbJYmVIc',
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
 )
