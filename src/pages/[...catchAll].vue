3ef4daa9563680ffbbc6462e353044366cf8348f
Mkgeek23 <emkgeek@gmail.com>
2025-03-12 09:14:49 +0100

    Addapt project to auto route

---
+++ src/pages/[...catchAll].vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+    <RouterLink to="/">HomePage</RouterLink>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,5 +6,7 @@ import { RouterLink } from 'vue-router'
   <div>
     <h1>Home Page</h1>
     <RouterLink to="/projects">Projects</RouterLink>
+    <br />
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
   </div>
 </template>
