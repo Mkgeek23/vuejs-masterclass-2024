09e7c56bbfabfe673d202d76567c28dd25df7b09
Mkgeek23 <emkgeek@gmail.com>
2025-03-12 13:37:30 +0100

    Add tasks page

--- supabase/migrations/20250312120050_tasks-schema.sql
+++ supabase/migrations/20250312120050_tasks-schema.sql
@@ -1,17 +1,15 @@
-drop table if exists projects;
-
-drop type if exists current_status;
-
-create type current_status as enum('in-progress', 'completed');
+drop table if exists tasks;
 
 create table 
-  projects (
+  tasks (
     id bigint primary key generated always as identity not null,
     created_at timestamp default now() not null,
     name text not null,
-    slug text unique not null,
     status current_status default 'in-progress' not null,
-    collaborators text default array[]::varchar[] not null
+    description text not null,
+    due_date date default null,
+    project_id bigint references projects (id) default null,
+    collaborators text array default array[]::varchar[] not null
   );
 
 --insert into projects (name, slug) values ('Project One', 'project-one');
