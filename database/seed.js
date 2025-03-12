f94566f3a9bc1c10e6083b710b4ed4778a34020b
Mkgeek23 <emkgeek@gmail.com>
2025-03-12 12:00:05 +0100

    Add fake initialize projects data

--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,20 @@
+/* eslint-env node */
+
 import { fakerEN_US as faker } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
 
 const personName = faker.person.fullName()
 
-console.log(personName)
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
+  await supabase.from('projects').insert({
+    name: name,
+    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collabolators: faker.helpers.arrayElements([1, 2, 3]),
+  })
+}
+
+await seedProjects()
